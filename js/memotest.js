6d6f4b93cdf685734dc0b425e3c785db6822b455
fedengarcia <fedengarcia11@hotmail.com>
2021-05-08 13:41:24 -0300

    metodo FlipCard

--- js/memotest.js
+++ js/memotest.js
@@ -4,19 +4,19 @@ class Memotest{
     }
 
     //Check de dificultad y reparto las cartas en el tablero dependiendo de la misma
-    armarTablero(dificultad){
-        if (dificultad == 'value1') {
+    armarTablero(){
+        if (this.dificultad == 'value1') {
             this.repartirCartas(11,6);
-        } else if (dificultad == 'value2') {
+        } else if (this.dificultad == 'value2') {
             this.repartirCartas(19,10);
-        }else if (dificultad == 'value3') {
+        }else if (this.dificultad == 'value3') {
             this.repartirCartas(27,14);
         }
     }
 
     //Disparador de inicio de juego
     iniciarJuego() {
-        this.armarTablero(this.dificultad);
+        this.armarTablero();
     }
 
     //Distribuyo las cartas en orden aleatorio
@@ -28,6 +28,7 @@ class Memotest{
         while(repartir <= cantidadCartas) {
             var numeroCarta = Math.floor((Math.random() * randomNumber) + 1);
             var count = 0;
+
             for (let index = 0; index < repetido.length; index++) {
                 if (repetido[index] == numeroCarta) {
                     count = count + 1;
@@ -40,54 +41,57 @@ class Memotest{
                 var divCartaOriginal = document.createElement('div');
                 divCartaOriginal.classList.add('carta');
                 divCartaOriginal.setAttribute('id',`carta-${numeroCarta}`);
+                
+
+                // var divSecretCard = document.createElement('div');
+                // divSecretCard.classList.add('cartaSecreta');
+                // divSecretCard.innerHTML = `<h2>?</h2>`;
 
                 // CUANDO SE EJECUTA ESTE EVENTO ? - No es en el onclick
                 divCartaOriginal.addEventListener('click',this.onClickFlipCard);
                 
-                var divSecretCard = document.createElement('div');
-                divSecretCard.classList.add('cartaSecreta');
-                divSecretCard.innerHTML = `<h2>?</h2>`;
 
                 if(cantidadCartas == 27){
-                    divCartaOriginal.innerHTML = `<img class="cartaDificil" src="../img/memo${numeroCarta}.png" alt="">`;
-                    divCartaOriginal.appendChild(divSecretCard);
+                    divCartaOriginal.innerHTML = `<div><img class="cartaDificil" src="../img/memo${numeroCarta}.png" alt=""></div><div class='cartaSecreta  animate__animated animate__bounceIn'><h2>?</h2></div>`;
                     tablero.appendChild(divCartaOriginal);
                     
                     repartir++;
                 }else{
-                    divCartaOriginal.innerHTML = `<img src="../img/memo${numeroCarta}.png" alt="">`;
-                    divCartaOriginal.appendChild(divSecretCard);
+                    divCartaOriginal.innerHTML = `<div><img src="../img/memo${numeroCarta}.png" alt=""></div><div class="cartaSecreta animate__animated animate__bounceIn"><h2>?</h2></div>`;
                     tablero.appendChild(divCartaOriginal);
                     
                     repartir++;
                 }
+                
                 divCartaOriginal.childNodes[0].style.visibility=('hidden');
 
             }
         }
     }
 
+    //Dar vuelta la carta
     onClickFlipCard(){
-        const ids = [];
-        console.log('Dificultad-->',this.dificultad);
-        if (this.dificultad == 'value1') {
-            for (let index = 0; index < 6; index++) {
-                ids.push(String(index+1));    
-            }
-        } else if (this.dificultad  == 'value2') {
-            for (let index = 0; index < 10; index++) {
-                ids.push(String(index+1));    
-            }
-        }else if (this.dificultad  == 'value3') {
-            for (let index = 0; index < 14; index++) {
-                ids.push(String(index+1));    
-            }
-        }
-        console.log(ids) 
-        // for (let index = 0; index < arrayCartas.length; index++) {
-        //     var carta = arrayCartas[index];
-        //     console.log(carta.getAttribute(id));
-        // }
+        console.log(this.id);
+        if (this.childNodes[0].style.visibility == 'visible'){
+            this.childNodes[0].style.visibility=('hidden');
+            this.childNodes[1].style.visibility=('visible');
+            this.childNodes[0].classList.remove('animate__animated');
+            this.childNodes[0].classList.remove('animate__flipInY');
+            this.childNodes[1].classList.add('animate__animated');
+            this.childNodes[1].classList.add('animate__bounceIn');
+            
+        }else{
+            this.childNodes[0].classList.add('animate__animated');
+            this.childNodes[0].classList.add('animate__flipInY');
+            this.childNodes[0].style.visibility=('visible');
+            this.childNodes[1].style.visibility=('hidden');
+            this.childNodes[1].classList.remove('animate__animated');
+            this.childNodes[1].classList.remove('animate__bounceIn');
+            
+        }   
+
+
+
     }
 
 
